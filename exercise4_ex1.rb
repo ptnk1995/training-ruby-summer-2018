76abcaceea9178288a0553ca0583149d123de548
at-vinhdo <thanhvinh0195.cntt@gmail.com>
2017-06-16 09:06:54 +0700

    exercise4 ex1

--- exercise4_ex1.rb
+++ exercise4_ex1.rb
@@ -1,4 +1,6 @@
 def centuryFromYear year
-    year / 100 + 1
+    century = (year % 100 == 0) ?  year / 100 : year / 100 + 1
 end
 p centuryFromYear 2000
+p centuryFromYear 2001
+p centuryFromYear 1701
